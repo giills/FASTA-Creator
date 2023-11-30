4926bb43f4d1d6424ded35e6964e80783e3dd9a7
julia <juliamgilley@gmail.com>
2023-11-30 05:04:09 -0500

    Moved find use case into its own folder and finished the committed files

---
+++ src/Use_Case/FindSequence/FindSequence.java
@@ -0,0 +1,13 @@
+package Use_Case.FindSequence;
+
+import Use_Case.FindSequence.FinderInputBoundary;
+import Use_Case.FindSequence.FinderInputData;
+
+public class FindSequence implements FinderInputBoundary{
+    
+    public FindSequence()
+
+    public void execute(FinderInputData inputData){
+
+    }
+}
