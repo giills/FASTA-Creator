bbb77260766da964ecde57874566f5fa177b5dd8
julia <juliamgilley@gmail.com>
2023-12-01 03:09:12 -0500

    Added UI for find and write

---
+++ src/App/FindWriteUseCaseFactory.java
@@ -0,0 +1,40 @@
+package App;
+
+import Adapter.Finder.FinderController;
+import Adapter.Finder.FinderPresenter;
+import Adapter.View.ViewModel;
+import Adapter.Writer.WriterController;
+import Adapter.Writer.WriterPresenter;
+import Data_Access.FinderDataAccessObject;
+import Use_Case.FindSequence.FindSequence;
+import Use_Case.FindSequence.FindSequenceDataAccessInterface;
+import Use_Case.FindSequence.FinderInputBoundary;
+import Use_Case.FindSequence.FinderOutputBoundary;
+import Use_Case.WriteEntry.WriteEntry;
+import Use_Case.WriteEntry.WriteEntryDataAccessInterface;
+import Use_Case.WriteEntry.WriterInputBoundary;
+import Use_Case.WriteEntry.WriterOutputBoundary;
+
+public class FindWriteUseCaseFactory {
+    private FindWriteUseCaseFactory(){};
+    public static View create(ViewManagerModel viewManagerModel, ViewModel viewModel, FinderDataAccessObject findDataAccessObject){
+            WriterController writerController = createWriteCase(viewManagerModel, viewModel, findDataAccessObject);
+            FinderController finderController = createFindCase(viewManagerModel, null, null, null);
+            return new View(finderController, viewModel, writerController);
+    }
+    private static WriterController createWriteCase(ViewManagerModel viewManagerModel, ViewModel viewModel, WriteEntryDataAccessInterface findDataAccessObject){
+        WriterOutputBoundary writerOutputBoundary = new WriterPresenter();
+
+        WriterInputBoundary writerInteractor = new WriteEntry(writerOutputBoundary, findDataAccessObject);
+
+        return new WriterController(writerInteractor);
+    }
+
+    private static FinderController createFindCase(ViewManagerModel viewManagerModel, ViewModel viewModel, FindSequenceDataAccessInterface findDataAccessObject, WriterController writerController){
+        FinderOutputBoundary finderOutputBoundary = new FinderPresenter(writerController);
+
+        FinderInputBoundary finderInteractor = new FindSequence(finderOutputBoundary, findDataAccessObject);
+
+        return new FinderController(finderInteractor);
+    }
+}
---
+++ src/App/Main.java
@@ -0,0 +1,49 @@
+package App;
+
+import java.awt.CardLayout;
+
+import javax.swing.*;
+
+import Adapter.View.ViewModel;
+import Data_Access.FinderDataAccessObject;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        JFrame application = new JFrame("Find and Write");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+        // view objects
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        // what view is showing
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        ViewModel viewModel = new ViewModel();
+        FinderDataAccessObject finderDataAccess = new FinderDataAccessObject();
+
+        View view = FindWriteUseCaseFactory.create(viewManagerModel, viewModel, finderDataAccess);
+        views.add(view, view.viewName);
+
+        /* to be added as UI for each use case is done
+        CompareView compareView = CompareUseCaseFactory.create(viewManagerModel, navigationViewModel, compareViewModel);
+        views.add(compareView, compareView.viewName);
+
+
+        PlayerSearchView playerSearchView = PlayerSearchUseCaseFactory.create(viewManagerModel, navigationViewModel, playerSearchViewModel, apiDataAccessObject, idSearchDataAccessOject);
+        views.add(playerSearchView, playerSearchView.viewName);
+        */
+
+
+        viewManagerModel.setActiveView(view.viewName);
+        viewManagerModel.firePropertyChanged();
+
+
+
+        application.pack();
+        application.setVisible(true);
+    }
+}
