2ac4229672004ff2f536cc9702cb786e814c578c
julia <juliamgilley@gmail.com>
2023-12-07 03:40:24 -0500

    Added functionality for searching for genes over multiple blast hits

--- src/Adapter/Finder/FinderPresenter.java
+++ src/Adapter/Finder/FinderPresenter.java
@@ -1,22 +1,15 @@
 package Adapter.Finder;
-import java.util.*;
 
 import Adapter.View.ViewModel;
 import Adapter.View.ViewState;
-import Adapter.Writer.WriterController;
-import App.ViewManagerModel;
 import Use_Case.FindSequence.FinderOutputBoundary;
 import Use_Case.FindSequence.FinderOutputData;
 
 public class FinderPresenter implements FinderOutputBoundary{
-    private final WriterController writerController;
     private ViewModel viewModel;
-    private ViewManagerModel viewManagerModel;
     
-    public FinderPresenter(WriterController writerController, ViewManagerModel viewManagerModel, ViewModel viewModel){
-        this.writerController = writerController;
+    public FinderPresenter(ViewModel viewModel){
         this.viewModel = viewModel;
-        this.viewManagerModel = viewManagerModel;
     }
 
     @Override
